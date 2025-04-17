39790fb57e4ba86a4554679bb89315d5a413b593
CR Hapuarachchi <chirantharavishka@gmail.com>
2025-04-17 10:09:10 +0530

    modified initialization script that includes both database setup and table creation

--- src/init-multiple-dbs.sh
+++ src/init-multiple-dbs.sh
@@ -2,13 +2,18 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    -- MLflow Database
+    -- Create MLflow Database
     CREATE DATABASE mlflow;
     CREATE USER mlflow WITH PASSWORD 'mlflow';
     GRANT ALL PRIVILEGES ON DATABASE mlflow TO mlflow;
-    
-    -- Notification Service Database
+
+    -- Create Notification Service Database with Table
     CREATE DATABASE "notify-data";
-    CREATE USER postgres WITH PASSWORD '12345q';
     GRANT ALL PRIVILEGES ON DATABASE "notify-data" TO postgres;
+    
+    \c "notify-data"
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INT PRIMARY KEY,
+        email VARCHAR(255) NOT NULL UNIQUE
+    );
 EOSQL
