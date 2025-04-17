53db17b1d2f5d71bda4882eb17d1f1b521713246
CR Hapuarachchi <chirantharavishka@gmail.com>
2025-04-17 12:52:53 +0530

    remove notification service

--- src/init-multiple-dbs.sh
+++ src/init-multiple-dbs.sh
@@ -2,18 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    -- Create MLflow Database
-    CREATE DATABASE mlflow;
     CREATE USER mlflow WITH PASSWORD 'mlflow';
+    CREATE DATABASE mlflow;
     GRANT ALL PRIVILEGES ON DATABASE mlflow TO mlflow;
-
-    -- Create Notification Service Database with Table
-    CREATE DATABASE "notify-data";
-    GRANT ALL PRIVILEGES ON DATABASE "notify-data" TO postgres;
-    
-    \c "notify-data"
-    CREATE TABLE IF NOT EXISTS users (
-        user_id INT PRIMARY KEY,
-        email VARCHAR(255) NOT NULL UNIQUE
-    );
 EOSQL
